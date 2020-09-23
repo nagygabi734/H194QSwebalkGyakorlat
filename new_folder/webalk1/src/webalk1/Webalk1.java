b50cd98b30acf560055a60d5c4b1840de1d2dc30
Nagy Gabor <gabinagy0520@gmail.com>
2020-09-23 15:39:47 +0200

    Ez az összeadás

--- new_folder/webalk1/src/webalk1/Webalk1.java
+++ new_folder/webalk1/src/webalk1/Webalk1.java
@@ -1,9 +1,31 @@
 package webalk1;
 
+import java.util.Scanner;
+
 public class Webalk1 {
 
 	public static void main(String[] args) {
-		System.out.println("Alma");
+		int a;
+		int b;
+		System.out.println("1: ");
+		a=Integer.parseInt(beolvas());
+		System.out.println("2: ");
+		b=Integer.parseInt(beolvas());
+		System.out.println("jel:");
+		String j=beolvas();
+		
+		switch (j) {
+		case "+": System.out.println(a+b);break;
+		}
 	}
-
+	
+	public static String beolvas() {
+		Scanner sc = new Scanner(System.in);
+		String x;
+		x=sc.nextLine();
+		sc.close();
+		return x;
+				
+	}
+	
 }
